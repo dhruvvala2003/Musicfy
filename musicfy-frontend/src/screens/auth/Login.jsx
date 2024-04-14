1b33f0312d9c2673739f6bc71e78a8427d45d09c
Vala Dhruv <dkvala2003@gmail.com>
2024-04-14 09:15:57 +0530

    authenticatiob login sign up page implemented

--- musicfy-frontend/src/screens/auth/Login.jsx
+++ musicfy-frontend/src/screens/auth/Login.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import './loginCss.css'
+import apiClient, { loginEndpoint } from '../../spotify'
 const Login = () => {
+   
   return (
-    <div>
-      
-    </div>
+    
+    <div className='login-page'>
+    <img src="https://imgs.search.brave.com/OxCiBszqfjx6KQrUTgUaBrHLOlOGkcwWVRQQUL6MKwk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/bG9nby53aW5lL2Ev/bG9nby9TcG90aWZ5/L1Nwb3RpZnktTG9n/by53aW5lLnN2Zw.svg"
+    
+         alt='logo-spotify'
+         className='logo'
+     />
+    <a href={loginEndpoint}>
+        <div className='login-btn'>Login</div>
+    </a>
+</div>
   )
 }
 
